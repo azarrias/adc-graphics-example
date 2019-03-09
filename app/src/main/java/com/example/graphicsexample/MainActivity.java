3228bc9252214e21b23a6b2439cf7fa12fe87bab
azarrias <adolfo.zarrias@gmail.com>
2019-03-09 22:36:19 +0100

    Draw a blue circle using the Paint class

--- app/src/main/java/com/example/graphicsexample/MainActivity.java
+++ app/src/main/java/com/example/graphicsexample/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.graphicsexample;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new ExampleView(this));
+    }
+
+    public class ExampleView extends View {
+        public ExampleView (Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            Paint brush = new Paint();
+            brush.setColor(Color.BLUE);
+            brush.setStrokeWidth(8);
+            brush.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(150, 150, 100, brush);
+        }
     }
 }
