3cbce96d4fedee2a47d9e811419169be87734eef
azarrias <adolfo.zarrias@gmail.com>
2019-03-10 20:10:28 +0100

    Paint text on a circular path

--- app/src/main/java/com/example/graphicsexample/MainActivity.java
+++ app/src/main/java/com/example/graphicsexample/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onDraw(Canvas canvas) {
+            /* Paint circle using color defined in resource files
             Paint brush = new Paint();
             brush.setColor(Color.BLUE);
             brush.setStrokeWidth(8);
@@ -31,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
             //brush.setColor(Color.argb(127, 255, 0, 0));
             brush.setColor(getResources().getColor(R.color.colorCircle));
             canvas.drawCircle(150, 250, 100, brush);
+            */
+
+            Path stroke = new Path();
+            stroke.addCircle(150, 150, 100, Path.Direction.CCW);
+            canvas.drawColor(Color.WHITE);
+            Paint brush = new Paint();
+            brush.setColor(Color.BLUE);
+            brush.setStrokeWidth(8);
+            brush.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(stroke, brush);
+            brush.setStrokeWidth(1);
+            brush.setStyle(Paint.Style.FILL);
+            brush.setTextSize(25);
+            brush.setTypeface(Typeface.SANS_SERIF);
+            canvas.drawTextOnPath("App development for Android", stroke, 10, 40, brush);
         }
     }
 }
